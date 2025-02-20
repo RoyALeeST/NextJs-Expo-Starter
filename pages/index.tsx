617031e3090f7bc3dc10af1721ce46aac7d30e9c
RoyALeeST <rodrigo.villalobos.san@gmail.com>
2025-02-20 17:51:39 -0600

    feat(init): initial nextjs expo config

---
+++ pages/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome to Expo + Next.js 👋</Text>
+      <Text style={styles.subtitle}>This is a test</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  subtitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+});
